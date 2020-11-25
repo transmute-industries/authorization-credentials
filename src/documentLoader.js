ef40b655e113e4801bc1efebfbbf7229c263c94f
Orie Steele <orie@transmute.industries>
2020-11-25 12:29:10 -0600

    chore: example

---
+++ src/documentLoader.js
@@ -0,0 +1,33 @@
+const {
+  documentLoaderFactory,
+  contexts,
+} = require("@transmute/jsonld-document-loader");
+
+const { driver } = require("@transmute/did-key-web-crypto");
+
+const golem = documentLoaderFactory.pluginFactory.build({
+  contexts: {
+    ...contexts.W3C_Decentralized_Identifiers,
+    ...contexts.W3C_Verifiable_Credentials,
+    ...contexts.W3ID_Security_Vocabulary,
+  },
+});
+
+golem.addContext({
+  "https://www.w3.org/2018/credentials/v2": require("../contexts/credentials-v2.json"),
+});
+
+golem.addResolver({
+  "did:key:": {
+    resolve: async (uri) => {
+      const { didDocument } = await driver.resolve(uri, {
+        accept: "application/did+json",
+      });
+      return didDocument;
+    },
+  },
+});
+
+const documentLoader = golem.buildDocumentLoader();
+
+module.exports = { documentLoader };
